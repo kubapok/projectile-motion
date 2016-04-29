24cadaddf1d7e1a3a5586e6f93bfc4bd1b6d341a
kubapok <jakubpokrywka@gmail.com>
2016-04-29 14:24:19 +0200

    dziala ladnie szablon, bede robil teraz klase

--- rzut.py
+++ rzut.py
@@ -1,14 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import math
 
 fig, ax = plt.subplots()
-ax.set_xlim(0, 5)
+ax.set_xlim(0, 100)
 ax.set_ylim(-1.1, 1.1)
 
-def animate(i):
-	x = np.arange(0,(0.01+0.01*i)*np.pi, 0.01)
-	line = ax.plot(x, np.sin(x), color = 'b')
+
+kappa = 2
+
+def xpos(t):
+	return t
+
+def ypos(t):
+	return np.sin(t/20)
+
+
+def animate(t):
+	x = np.arange(0,t, 0.01)
+	line = ax.plot(x, ypos(x), color = 'b')
 	return line
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, 100),
