b244b5bbd3998cbac79428261bd959179257def4
kubapok <jakubpokrywka@gmail.com>
2016-04-29 14:06:24 +0200

    ladnie wyglada juz cosinus

--- rzut.py
+++ rzut.py
@@ -3,24 +3,15 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
-
-
-x = np.arange(0,2*np.pi, 0.01)        
-ax.set_ylim(-1.1, 1.1)
 ax.set_xlim(0, 5)
+ax.set_ylim(-1.1, 1.1)
 
 def animate(i):
 	x = np.arange(0,(0.01+0.01*i)*np.pi, 0.01)
-	line = ax.plot(x, np.sin(x))
+	line = ax.plot(x, np.sin(x), color = 'b')
 	return line
 
-def init():
-	x = np.arange(0,(0.01)*np.pi, 0.01)
-	line = ax.plot(x, np.cos(x))
-	return line
-
-
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), 
-init_func=init,
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 100),
     interval=25, blit=True)
+
 plt.show()
